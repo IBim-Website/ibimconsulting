e432af4667a9ebdf505412798166709114ea2363
JJ Labajo <joejunelabajojr@gmail.com>
2026-04-07 13:47:28 +0800

    allow promocode during checkout

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -107,6 +107,7 @@ export async function POST(request) {
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: checkoutMode, // Automatically set to 'payment' or 'subscription'
+      allow_promotion_codes: true,
       success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`, 
       cancel_url: `${origin}/cart`,
     });
