27d8672ca5f43f02a6e30d215d3a2567b76185e9
JJ Labajo <joejunelabajojr@gmail.com>
2026-04-14 20:00:01 +0800

    updated new webhook secret for custom page checkout

--- app/api/stripe-webhook-custom-page/route.js
+++ app/api/stripe-webhook-custom-page/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET; 
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET2; 
 
 async function getBackofficeToken() {
   const authEndpoint = 'https://backoffice.ibimconsulting.com.au/api/authenticate';
